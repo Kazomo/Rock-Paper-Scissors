5acf618e5e9edac9c8f94cf5c1f9fcd339bb9e20
Mohammad <m.k.falahati@gmail.com>
2020-05-28 23:10:21 +0430

    complete the project and fixed the bugs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//random number between 0 and max
+//random number function to choose between rock paper scissors
 function computerPlay() {
   let randomNumber = Math.floor(Math.random() * Math.floor(3));
   switch (randomNumber) {
@@ -13,28 +13,31 @@ function computerPlay() {
       break;
   }
 }
-
+//score variables
 let tieCounter = 0;
 let playerScore = 0;
 let computerScore = 0;
 let message;
-function playRound(playerSelection, computerSelection) {
-  playerSelection = prompt("Enter your choice: ").toLowerCase();
-  computerSelection = computerPlay();
 
+//play function
+function playRound(playerSelection, computerSelection) {
+  playerSelection = prompt("Rock, Paper or Scissors? ").toLowerCase();
+
+  //check the entered value to see it's correct
   if (
     playerSelection === "rock" ||
     playerSelection === "paper" ||
     playerSelection === "scissors"
   ) {
-    return playerSelection;
+    playerSelection = playerSelection;
   } else {
-    message = "Please select one of these optios:\n Rock, Paper, Scissors";
+    message = "Please select one of these optios:\nRock, Paper, Scissors";
   }
-
+  computerSelection = computerPlay();
+  //the main conditional of the game
   switch (playerSelection) {
     case computerSelection:
-      message = "tie";
+      message = "The game is a tie";
       return tieCounter++;
       break;
     case "rock":
@@ -72,22 +75,30 @@ function playRound(playerSelection, computerSelection) {
       }
   }
 }
+//using the playRound function to play a 5 round game and show the result
 function game() {
   let output;
   playRound();
-  output = `${message}. Player Score=${playerScore} Computer Score=${computerScore} Tie=${tieCounter}`;
+  output = `${message}\nPlayerScore=${playerScore} ComputerScore=${computerScore} Tie=${tieCounter}`;
   console.log(output);
   playRound();
-  output = `${message}. Player Score=${playerScore} Computer Score=${computerScore} Tie=${tieCounter}`;
+  output = `${message}\nPlayerScore=${playerScore} ComputerScore=${computerScore} Tie=${tieCounter}`;
   console.log(output);
   playRound();
-  output = `${message}. Player Score=${playerScore} Computer Score=${computerScore} Tie=${tieCounter}`;
+  output = `${message}\nPlayerScore=${playerScore} ComputerScore=${computerScore} Tie=${tieCounter}`;
   console.log(output);
   playRound();
-  output = `${message}. Player Score=${playerScore} Computer Score=${computerScore} Tie=${tieCounter}`;
+  output = `${message}\nPlayerScore=${playerScore} ComputerScore=${computerScore} Tie=${tieCounter}`;
   console.log(output);
   playRound();
-  output = `${message}. Player Score=${playerScore} Computer Score=${computerScore} Tie=${tieCounter}`;
+  output = `${message}\nPlayerScore=${playerScore} ComputerScore=${computerScore} Tie=${tieCounter}`;
   console.log(output);
+  if (playerScore > computerScore) {
+    console.log("congratulation, you win!");
+  } else if (computerScore > playerScore) {
+    console.log("Sorry, you have Lost!");
+  } else {
+    console.log("It's a tie");
+  }
 }
 game();
